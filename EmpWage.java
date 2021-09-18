94e20e58dddece2862467116069106561f2ee7a2
Vineesha-1 <vineesha.algam1218@gmail.com>
2021-09-18 15:29:43 +0530

    ADD:switch case

--- EmpWage.java
+++ EmpWage.java
@@ -1,10 +1,10 @@
 
 public class EmpWage {
+	public static final int IS_PART_TIME=1;
+	public static final int IS_FULL_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
 
 	public static void main(String[] args) {
-		int IS_FULL_TIME=2;
-		int IS_PART_TIME=1;
-		int EMP_RATE_PER_HOUR=20;
 		int empHrs=0;
 		int empWage=0;
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
@@ -14,23 +14,23 @@ public class EmpWage {
 		 // Print random integers
 		 System.out.println("Random Integers: "+empCheck);
 		 //System.out.println("Random Integers: "+rand_int2);
-		if(empCheck==IS_PART_TIME)
-			 {
-			     empHrs=4;	  
-				   
-			 }
-			 else if(empCheck==IS_FULL_TIME)
-			 {
-				  empHrs=8;
-			 }
-			 else 
-			 {
-			      empHrs=0;	  
-				   System.out.println("employee absent");
-			 }
+		 switch (empCheck) {
+		 case 1:   empHrs=8;
+		          System.out.print("full time");
+		          break;
+		 case 2:  empHrs=4;
+		          System.out.print("part time");
+		          break;
+		 case 0:  empHrs=0;
+		          System.out.println("absent");
+		          break;
+		 
+		 default:empHrs=0;
+		          break;
+		 }
 		 empWage=empHrs*EMP_RATE_PER_HOUR;
 		 System.out.println("emp wage"+empWage);
 
 	}
-
 }
+
