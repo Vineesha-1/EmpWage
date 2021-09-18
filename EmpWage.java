003db3e80e76cd768ccfa4a7c1859ac9d23f05af
Vineesha-1 <vineesha.algam1218@gmail.com>
2021-09-18 15:50:52 +0530

    ADD:calculating for 20 days per month

--- EmpWage.java
+++ EmpWage.java
@@ -1,36 +1,44 @@
 
 public class EmpWage {
 
-	public static void main(String[] args) {
-		int IS_FULL_TIME=2;
-		int IS_PART_TIME=1;
-		int EMP_RATE_PER_HOUR=20;
-		int empHrs=0;
-		int empWage=0;
-		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-		int empCheck = (int) (Math.floor(Math.random()*10)%2);;
-		// int rand_int2 = rand.nextInt(1000);
+	//private static final int NUM_OF_WORKING_DAYS = 0;
+	public static final int IS_PART_TIME=1;
+	public static final int IS_FULL_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
+	public static final int NUM_OF_WORKING_DAYS=20;
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			int empHr;
+			int empWage;
+			int totalEmpWage = 0;
+	System.out.print("Welcome to Employee Wage Computation Program on Master Branch");
+	for(int day=0;day<NUM_OF_WORKING_DAYS;day++)
+	{
+	int empCheck = (int) (Math.floor(Math.random()*10)%3);;
+	System.out.println("Random Integers: "+empCheck);
+	 //System.out.println("Random Integers: "+rand_int2);
 
-		 // Print random integers
-		 System.out.println("Random Integers: "+empCheck);
-		 //System.out.println("Random Integers: "+rand_int2);
-		if(empCheck==IS_PART_TIME)
-			 {
-			     empHrs=4;	  
-				   
-			 }
-			 else if(empCheck==IS_FULL_TIME)
-			 {
-				  empHrs=8;
-			 }
-			 else 
-			 {
-			      empHrs=0;	  
-				   System.out.println("employee absent");
-			 }
-		 empWage=empHrs*EMP_RATE_PER_HOUR;
-		 System.out.println("emp wage"+empWage);
-
+	 switch (empCheck) {
+	 case 1:   empHr=8;
+	          System.out.print("full time");
+	          break;
+	 case 2:  empHr=4;
+	          System.out.print("part time");
+	          break;
+	 case 0:  empHr=0;
+	          System.out.println("absent");
+	          break;
+	 
+	 default:empHr=0;
+	          break;
+	}
+	 empWage=empHr*EMP_RATE_PER_HOUR;
+	 totalEmpWage+=empWage;
+	System.out.println("emp wage"+empWage);
+		
+		}
+	System.out.println("Total emp wage"+totalEmpWage);
+		}
 	}
 
-}
