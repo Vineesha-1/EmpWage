6ca19821f273632a4da5f0756b92419ce5357129
Vineesha-1 <vineesha.algam1218@gmail.com>
2021-09-18 14:51:46 +0530

    ADD:Calculation of daily EmpWage

--- EmpWage.java
+++ EmpWage.java
@@ -2,6 +2,10 @@
 public class EmpWage {
 
 	public static void main(String[] args) {
+		int IS_FULL_TIME=1;
+		int EMP_RATE_PER_HOUR=20;
+		int empHrs=0;
+		int empWage=0;
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 		int empCheck = (int) (Math.floor(Math.random()*10)%2);;
 		// int rand_int2 = rand.nextInt(1000);
@@ -13,12 +17,16 @@ public class EmpWage {
 		 if(empCheck==1)
 		 {
 			   System.out.println("employee present");
+			   empHrs=8;
 			   
 		 }
 		 else
 		 {
 			   System.out.println("employee absent");
+			   empHrs=0;
 		 }
+		 empWage=empHrs*EMP_RATE_PER_HOUR;
+		 System.out.println("emp wage"+empWage);
 
 	}
 
