8dfa3e6d52f8f6742e7f9e908c80592817a94f18
Vineesha-1 <vineesha.algam1218@gmail.com>
2021-09-18 14:36:43 +0530

    ADD:Check employee present or absent

--- EmpWage.java
+++ EmpWage.java
@@ -3,6 +3,22 @@ public class EmpWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+		int empCheck = (int) (Math.floor(Math.random()*10)%2);;
+		// int rand_int2 = rand.nextInt(1000);
+
+		 // Print random integers
+		 System.out.println("Random Integers: "+empCheck);
+		 //System.out.println("Random Integers: "+rand_int2);
+
+		 if(empCheck==1)
+		 {
+			   System.out.println("employee present");
+			   
+		 }
+		 else
+		 {
+			   System.out.println("employee absent");
+		 }
 
 	}
 
